78704a6020974bd7d29b7dd74217908377d09827
LAPTOP-6VVLGKOP\admin <wjdtn402@naver.com>
2021-01-08 15:43:33 +0900

    Python
    기초 코드들

---
+++ profile.py
@@ -0,0 +1,7 @@
+
+name = input('What is your name?')
+id = input('What is your ID?')
+email = input('Whatis your E-mail?')
+print(name)
+print(id)
+print(email)
---
+++ string.py
@@ -0,0 +1,16 @@
+a='I love Python'
+print(a)
+a= "I 'love' Python"
+print(a)
+text = """ 실패하는 것 보다
+도전하지 않는 것을
+두려워하라"""
+print(text)
+len(text)
+name = input('이름을 입력하세요')
+message = name + '님! 반갑습니다!'
+print(message)
+message = message*3
+print(message)
+message=f'{name}님~ 반갑습니다!'
+print(message)
